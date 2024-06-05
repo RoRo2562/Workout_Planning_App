2032bc5be72c7967d7aa8bee7dd5e60fbb027f64
Rohan <rsiv0010@student.monash.edu>
2024-06-05 18:04:44 +1000

    Added the ability to decode and encode food data so that meals can be stored under users, added the food item view so that users can view indivdual food ites

---
+++ Final-App/FoodSet.swift
@@ -0,0 +1,12 @@
+//
+//  FoodSet.swift
+//  Final-App
+//
+//  Created by Roro on 5/6/2024.
+//
+
+import UIKit
+
+class FoodSet: NSObject {
+
+}
---
+++ Final-App/Meals.swift
@@ -0,0 +1,12 @@
+//
+//  Meals.swift
+//  Final-App
+//
+//  Created by Roro on 5/6/2024.
+//
+
+import UIKit
+
+class Meals: NSObject {
+
+}
